d76ef6355b572f19af9fca926080697dfeac0b9e
Erick Gomez <erick0gomez98@gmail.com>
2018-06-29 14:45:06 -0500

    Error boundary try-catch errors
    Signed-off-by: Erick Gomez <erick0gomez98@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import styles from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+
 
 class App extends Component {
   state = {
@@ -64,12 +66,15 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              click={this.deletePersonHandler.bind(this, index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+            return (
+              <ErrorBoundary key={person.id}>
+                <Person
+                  click={this.deletePersonHandler.bind(this, index)}
+                  name={person.name}
+                  age={person.age}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}/>
+              </ErrorBoundary>
+            );
           })}
         </div>
       );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from './Person.css';
 
 const Person = (props) => {
+  const rnd = Math.random();
+  if (rnd > 0.7) {
+    throw new Error("ya valio");
+  }
   return (
     <div className={styles.Person}>
       <h1 onClick={props.click}>Hey I'm a {props.name} and i'm {props.age} years old </h1>
