b9358066c0d8183ef2c571c95f02e381b9ad29c6
Erick Gomez <erick0gomez98@gmail.com>
2018-06-29 13:48:50 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+import styles from './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      {id: 1, name: 'Max', age: 28},
+      {id: 2, name: 'Erick', age: 19}
+    ],
+    ohterState: 'Some other value',
+    showPersons: false
+  };
+
+  nameChangedHandler = (event, id) => {
+
+    // Es como la función de .map(), recibe una función para hacer una comparación, y regresa true o false si lo encontró
+    // o no en base a la comparación que le asignamos.
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // Hay que crear una copia del objeto de la persona que se encontro mediante el index para no modificar el original
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+
+    // Después creamos una copia de tod0 el array de personas, y en base al index, solo modificamos ese objeto (persona)
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({
+      persons: persons
+    });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+
+    this.setState({
+      showPersons: !doesShow
+    });
+  };
+
+  deletePersonHandler = (personIndex) => {
+    /* Hacerlo de esta manera esta mal, porque se esta modificando el valor original de this.state.persons y
+     no debe de ser asi, por lo tanto se tiene que crear una copia y ahora si manipularlo.
+    const persons = this.state.persons;
+    */
+
+    // Entonces para hacer una copia, simplemente se usa el spread operator (...)
+    const persons = [...this.state.persons];
+
+    persons.splice(personIndex, 1);
+    this.setState({
+      persons: persons
+    });
+  };
+
+  render() {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inheirt',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={this.deletePersonHandler.bind(this, index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+          })}
+        </div>
+      );
+
+      style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push(styles.red);
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push(styles.bold);
+    }
+
+    return (
+      <div className={styles.App}>
+        <h1>Hi I am a React App</h1>
+        <p className={classes.join(' ')}>This is really working</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons
+        </button>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Person.css';
+
+const Person = (props) => {
+  return (
+    <div className='Person'>
+      <h1 onClick={props.click}>Hey I'm a {props.name} and i'm {props.age} years old </h1>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
+    </div>
+  );
+};
+
+export default Person;
